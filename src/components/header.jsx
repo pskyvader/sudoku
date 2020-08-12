527a08b3fe99d4be4b0997a3d81a693715e8ff0a
Pablo <pskyvader@hotmail.com>
2020-08-11 19:50:58 -0500

    --- Auto-Git Commit ---

---
+++ src/components/header.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Container from '@material-ui/core/Container';
+import Slide from '@material-ui/core/Slide';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Zoom from '@material-ui/core/Zoom';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      position: 'fixed',
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+  }));
+
+  
+function HideOnScroll(props) {
+    const { children, window } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+}
+
+/**
+ * Injected by the documentation to work in an iframe.
+ * You won't need it on your project.
+ */
+HideOnScroll.propTypes = { children: PropTypes.element.isRequired, window: PropTypes.func, };
+
+
+function ScrollTop(props) {
+    const { children, window } = props;
+    const classes = useStyles();
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+        target: window ? window() : undefined,
+        disableHysteresis: true,
+        threshold: 100,
+    });
+
+    const handleClick = (event) => {
+        const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+        if (anchor) {
+            anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }
+    };
+
+    return (
+        <Zoom in={trigger}>
+            <div onClick={handleClick} role="presentation" className={classes.root}>
+                {children}
+            </div>
+        </Zoom>
+    );
+}
+
+
+ScrollTop.propTypes = {
+    children: PropTypes.element.isRequired,
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+  };
+
+export default function HideAppBar(props) {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <HideOnScroll {...props}>
+                <AppBar>
+                    <Toolbar>
+                        <Typography variant="h6">Scroll to Hide App Bar</Typography>
+                    </Toolbar>
+                </AppBar>
+            </HideOnScroll>
+            <Toolbar />
+            <Container>
+
+                {props.children}
+
+
+                <ScrollTop {...props}>
+                    <Fab color="secondary" size="small" aria-label="scroll back to top">
+                        <KeyboardArrowUpIcon />
+                    </Fab>
+                </ScrollTop>
+
+            </Container>
+        </React.Fragment>
+    );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => (
+    <React.Fragment>
+        <Typography variant="h3">Welcome Home!</Typography>
+        <Form />
+    </React.Fragment>
+);
