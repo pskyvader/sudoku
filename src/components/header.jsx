99385235b9651371a71dba9c73f0c57b6021964b
Pablo <pskyvader@hotmail.com>
2020-08-29 23:49:42 -0500

    --- Auto-Git Commit ---

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
@@ -16,65 +17,70 @@ import StarsIcon from '@material-ui/icons/Stars';
 
 
 import SudokuResolver from "../logic/SudokuResolver";
+import LocalStorage from "../logic/LocalStorage";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-    buttonstar:{
-        position:"absolute",
-        bottom:1,
-        right:1,
-        fontSize:"0.75rem"
-    },
-    button1: {
-        backgroundColor: theme.palette.info.main,
-        color: theme.palette.getContrastText(theme.palette.info.main),
+
+const buttoncolor = (pallete, getContrastText) => {
+    return {
+        backgroundColor: pallete.main,
+        color: getContrastText(pallete.main),
+        border: "none",
         '&:hover': {
-            backgroundColor: theme.palette.info.dark,
-            color: theme.palette.getContrastText(theme.palette.info.dark),
-        },
-    },
-    button2: {
-        backgroundColor: theme.palette.success.main,
-        color: theme.palette.getContrastText(theme.palette.success.main),
-        '&:hover': {
-            backgroundColor: theme.palette.success.dark,
-            color: theme.palette.getContrastText(theme.palette.success.dark),
-        },
-    },
-    button3: {
-        backgroundColor: theme.palette.warning.main,
-        color: theme.palette.getContrastText(theme.palette.warning.main),
-        '&:hover': {
-            backgroundColor: theme.palette.warning.dark,
-            color: theme.palette.getContrastText(theme.palette.warning.dark),
-        },
-    },
-    button4: {
-        backgroundColor: theme.palette.error.main,
-        color: theme.palette.getContrastText(theme.palette.error.main),
-        '&:hover': {
-            backgroundColor: theme.palette.error.dark,
-            color: theme.palette.getContrastText(theme.palette.error.dark),
+            backgroundColor: pallete.dark,
+            color: getContrastText(pallete.dark),
+            border: "none",
         },
     }
-}));
+}
+
+
+const useStyles = makeStyles((theme) => {
+    const { info, success, warning, error, getContrastText } = theme.palette;
+
+    return {
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flexGrow: 1,
+        },
+        buttonstar: {
+            position: "absolute",
+            bottom: 1,
+            right: 1,
+            fontSize: "0.75rem"
+        },
+        button1: buttoncolor(info, getContrastText),
+        button2: buttoncolor(success, getContrastText),
+        button3: buttoncolor(warning, getContrastText),
+        button4: buttoncolor(error, getContrastText)
+    }
+
+}
+
+
+);
 
 export default function ButtonAppBar(props) {
     const classes = useStyles();
+    const [Difficulty, setDifficulty] = React.useState(LocalStorage.get("difficulty", 45));
     const { board } = props;
 
     function ResetBoard(n) {
         const newboard = new SudokuResolver(n);
+        setDifficulty(n);
         board.RestoreBoard(newboard.CloneBoard());
     }
+    const Save = () => {
+        LocalStorage.set("difficulty", Difficulty);
+    }
+    React.useEffect(() => {
+        window.addEventListener("beforeunload", Save);
+        return () => window.removeEventListener("beforeunload", Save);
+    });
     return (
         <div className={classes.root}>
             <AppBar position="fixed">
@@ -83,13 +89,24 @@ export default function ButtonAppBar(props) {
                         <MenuIcon />
                     </IconButton> */}
                     <Typography variant="h6" className={classes.title}> Sudoku </Typography>
-                    <Typography variant="h6" className={classes.menuButton}> New </Typography>
 
-                    <ButtonGroup variant="contained" >
-                        <Button classes={{ root: classes.button1 }} onClick={() => ResetBoard(63)}><EmojiEmotionsIcon /> <StarsIcon className={classes.buttonstar}/></Button>
-                        <Button className={classes.button2} onClick={() => ResetBoard(45)}><InsertEmoticonIcon /></Button>
-                        <Button className={classes.button3} onClick={() => ResetBoard(36)}><FaceIcon /></Button>
-                        <Button className={classes.button4} onClick={() => ResetBoard(27)}><MoodBadIcon /></Button>
+                    <ButtonGroup>
+                        <Button className={classes.button1} onClick={() => ResetBoard(63)}>
+                            <EmojiEmotionsIcon />
+                            {Difficulty === 63 ? <StarsIcon className={classes.buttonstar} /> : ""}
+                        </Button>
+                        <Button className={classes.button2} onClick={() => ResetBoard(45)}>
+                            <InsertEmoticonIcon />
+                            {Difficulty === 45 ? <StarsIcon className={classes.buttonstar} /> : ""}
+                        </Button>
+                        <Button className={classes.button3} onClick={() => ResetBoard(36)}>
+                            <FaceIcon />
+                            {Difficulty === 36 ? <StarsIcon className={classes.buttonstar} /> : ""}
+                        </Button>
+                        <Button className={classes.button4} onClick={() => ResetBoard(27)}>
+                            <MoodBadIcon />
+                            {Difficulty === 27 ? <StarsIcon className={classes.buttonstar} /> : ""}
+                        </Button>
                     </ButtonGroup>
 
                 </Toolbar>
