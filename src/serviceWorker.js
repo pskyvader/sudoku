a4524b8a6ae78c571d43248e11e7955615303108
Pablo <pskyvader@hotmail.com>
2020-09-02 15:07:20 -0500

    Sudoku created

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -22,11 +22,9 @@ const isLocalhost = Boolean(
 
 export function register(config) {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-        console.log("b");
         // The URL constructor is available in all browsers that support SW.
         const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
         if (publicUrl.origin !== window.location.origin) {
-            console.log("c");
             // Our service worker won't work if PUBLIC_URL is on a different origin
             // from what our page is served on. This might happen if a CDN is used to
             // serve assets; see https://github.com/facebook/create-react-app/issues/2374
@@ -34,8 +32,6 @@ export function register(config) {
         }
 
         window.addEventListener('load', () => {
-
-            console.log("d");
             const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
             if (isLocalhost) {
