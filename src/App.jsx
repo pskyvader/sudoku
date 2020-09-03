a1273a7a92f5083531a230879085793c9556abe0
Pablo <pskyvader@hotmail.com>
2020-09-03 18:57:11 -0500

    --- Auto-Git Commit ---

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-// import Header from './components/header';
+import Header from './components/header';
 // import Home from './pages/Home';
 import './App.css';
 
@@ -11,19 +11,23 @@ const cacheboard = LocalStorage.get("sudoku_board", null);
 const baseboard = new SudokuResolver(45, cacheboard);
 
 
-const renderLoader = () => <p>Loading</p>;
+const renderLoader = () => <p>...</p>;
 
-const Header = lazy(() => import('./components/header'));
+//const Header = lazy(() => import('./components/header'));
 const Home = lazy(() => import('./pages/Home'));
 
 
+
+
 const App = () => (
-    <Suspense fallback={renderLoader()}>
-      <Header board={baseboard}>
-            <Home board={baseboard} />
+    // <Suspense fallback={renderLoader()}>
+        <Header board={baseboard}>
+            <Suspense fallback={renderLoader()}>
+                <Home board={baseboard} />
+            </Suspense>
         </Header>
-    </Suspense>
-  )
+    // </Suspense>
+)
 
 // function App() {
 //     return (
