5336ed1845b6c05b918fe0563485844702701df1
Pablo <pskyvader@hotmail.com>
2020-09-17 12:17:52 -0500

    --- Auto-Git Commit ---

--- src/components/UseServiceWorker.jsx
+++ src/components/UseServiceWorker.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import Slide from '@material-ui/core/Slide';
 import Snackbar from '@material-ui/core/Snackbar';
 import Alert from '@material-ui/lab/Alert';
-
+import CloseIcon from '@material-ui/icons/Close';
 
 import * as serviceWorker from '../serviceWorker';
 
 const UseServiceWorker = () => {
-    const [Message, setMessage] = React.useState("UPDATE");
+    const [Message, setMessage] = React.useState("");
     const [waitingServiceWorker, setWaitingServiceWorker] = React.useState(null);
+    const [installPrompt, setinstallPrompt] = React.useState(null);
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
             return;
@@ -22,6 +24,20 @@ const UseServiceWorker = () => {
             // We send the SKIP_WAITING message to tell the Service Worker
             // to update its cache and flush the old one
             waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
+        }
+        setMessage("");
+    };
+    const handleInstall = () => {
+        if (installPrompt) {
+            installPrompt.prompt();
+            // Wait for the user to respond to the prompt
+            installPrompt.userChoice.then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the install prompt');
+                } else {
+                    console.log('User dismissed the install prompt');
+                }
+            });
         }
         setMessage("");
     };
@@ -54,6 +70,17 @@ const UseServiceWorker = () => {
         }
     }, [waitingServiceWorker]);
 
+    React.useEffect(() => {
+        window.addEventListener('beforeinstallprompt', (e) => {
+            // Prevent the mini-infobar from appearing on mobile
+            e.preventDefault();
+            // Stash the event so it can be triggered later.
+            setinstallPrompt(e);
+        });
+    }, []);
+
+
+
     const transition = (props) => <Slide {...props} direction="up" />;
     return (
         <Snackbar
@@ -62,13 +89,26 @@ const UseServiceWorker = () => {
             anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
             TransitionComponent={transition}>
             {Message === "OFFLINE" ?
-                <Alert elevation={6} variant="filled" onClose={handleClose} severity="success"> Offline Mode Available! </Alert>
-                :
-                <Alert elevation={6} variant="filled" onClose={handleClose} severity="info"
+                <Alert elevation={6} variant="filled" severity="success"
                     action={
                         <React.Fragment>
-                        <Button color="inherit" size="small" onClick={handleUpdate}> UPDATE </Button>
-                        <Button color="inherit" size="small" onClick={handleClose}> CLOSE </Button>
+                            {installPrompt && <Button color="inherit" size="small" onClick={handleInstall}> INSTALL </Button>}
+                            <IconButton color="inherit" size="small" onClick={handleClose}>
+                                <CloseIcon fontSize="small" />
+                            </IconButton>
+                        </React.Fragment>
+                    }
+                >
+                    Offline Mode Available! Now you can play even if you're offline.
+                </Alert>
+                :
+                <Alert elevation={6} variant="filled" severity="info"
+                    action={
+                        <React.Fragment>
+                            <Button color="inherit" size="small" onClick={handleUpdate}> UPDATE </Button>
+                            <IconButton color="inherit" size="small" onClick={handleClose}>
+                                <CloseIcon fontSize="small" />
+                            </IconButton>
                         </React.Fragment>
                     }
                 >
